c07f88870b55b85fd245a691a6a86ed57fb7a871
Nathaniel Havim <nhavim123@gmail.com>
2023-05-08 16:40:26 +0000

    all changes

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import altair as alt
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformvers import AutoTokenizer, AutoModelForSequenceClassification
 from PIL import Image
 import base64
 
